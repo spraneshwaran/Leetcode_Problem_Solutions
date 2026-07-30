94d1d4ea4470284891dd29fc4ec87ff3e33fb4d6
Praneshwaran S <praneshwaransv@gmail.com>
2026-07-30 23:26:43 +0530

    Time: 45 ms (26.39%) | Memory: 47.1 MB (40.56%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -4,10 +4,7 @@ class Solution {
         {
             for(int j=i+1;j<nums.length;j++)
             {
-                if(nums[i]+nums[j] == target)
-                {
-                    return new int[]{i,j};
-                }
+                if(nums[i]+nums[j] == target) return new int[]{i,j};
             }
         }
         return new int[]{};
