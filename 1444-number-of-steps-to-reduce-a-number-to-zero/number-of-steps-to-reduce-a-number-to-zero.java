5ac5e30e31a7d5e240a9d7fa0e65227d073dc5c9
Praneshwaran S <praneshwaransv@gmail.com>
2026-08-01 15:14:35 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (38.22%) - LeetSync

---
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numberOfSteps(int num) {
+        int ct=0;
+        while(num!=0){
+            if((num & 1)!=1) num=num>>1;
+            else num--;
+            ct++;
+        }
+        return ct;
+    }
+}
