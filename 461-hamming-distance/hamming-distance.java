1f43e98eac3446d132b56a8a080c009b60cabd00
Praneshwaran S <praneshwaransv@gmail.com>
2026-08-01 10:31:18 +0530

    Time: 1 ms (6.27%) | Memory: 41.9 MB (90.03%) - LeetSync

--- 461-hamming-distance/hamming-distance.java
+++ 461-hamming-distance/hamming-distance.java
@@ -1,6 +1,11 @@
 class Solution {
     public int hammingDistance(int x, int y) {
         x=x^y;
-        return Integer.bitCount(x);
+        String s=Integer.toBinaryString(x);
+        int ct=0;
+        for(int i=0;i<s.length();i++){
+            if(Integer.valueOf(s.charAt(i)-'0') == 1) ct++;
+        }
+        return ct;
     }
 }
