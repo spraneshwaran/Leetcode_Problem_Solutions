0e48e643df7eea091b253911218c8933d12ad724
Praneshwaran S <praneshwaransv@gmail.com>
2026-08-06 10:43:09 +0530

    Time: 1 ms (66.28%) | Memory: 46.3 MB (67.57%) - LeetSync

--- 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.java
+++ 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.java
@@ -1,14 +1,13 @@
 class Solution {
     public int findSpecialInteger(int[] arr) {
-        int val=arr[0];
         int count=1;
         for(int i=1;i<arr.length;i++){
             if(arr[i]==arr[i-1]) count++;
             else count=1;
             if(count>arr.length/4){
-                val=arr[i];
+                return arr[i];
             }
         }
-        return val;
+        return arr[0];
     }
 }
