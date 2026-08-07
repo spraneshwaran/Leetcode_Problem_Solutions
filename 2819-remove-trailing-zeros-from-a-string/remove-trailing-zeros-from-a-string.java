0990bb7e2b98e11a51153db3a2f66b10affb8eb2
Praneshwaran S <praneshwaransv@gmail.com>
2026-08-07 16:06:59 +0530

    Time: 1 ms (100.00%) | Memory: 46.3 MB (69.96%) - LeetSync

--- 2819-remove-trailing-zeros-from-a-string/remove-trailing-zeros-from-a-string.java
+++ 2819-remove-trailing-zeros-from-a-string/remove-trailing-zeros-from-a-string.java
@@ -1,16 +1,9 @@
 class Solution {
     public String removeTrailingZeros(String num) {
-        List<Character> list=new ArrayList<>();
-        for(char c:num.toCharArray()){
-            list.add(c);
+        int n=num.length()-1;
+        while(n>=0 && num.charAt(n)=='0'){
+            n--;
         }
-        while(list.get(list.size()-1)=='0'){
-            list.removeLast();
-        }
-        StringBuilder sb=new StringBuilder();
-        for(char c:list){
-            sb.append(c);
-        }
-        return sb.toString();
+        return num.substring(0,n+1);
     }
 }
