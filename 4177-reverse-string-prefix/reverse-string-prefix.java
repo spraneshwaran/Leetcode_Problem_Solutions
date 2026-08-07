849ce895fa13f346b3c03b3a36a49f9b1b748443
Praneshwaran S <praneshwaransv@gmail.com>
2026-08-07 16:39:27 +0530

    Time: 2 ms (46.76%) | Memory: 44.9 MB (24.79%) - LeetSync

--- 4177-reverse-string-prefix/reverse-string-prefix.java
+++ 4177-reverse-string-prefix/reverse-string-prefix.java
@@ -1,7 +1,6 @@
 class Solution {
     public String reversePrefix(String s, int k) {
         StringBuilder sb=new StringBuilder(s.substring(0,k));
-        s=sb.reverse().toString()+s.substring(k);
-        return s;
+        return sb.reverse().toString()+s.substring(k);
     }
 }
